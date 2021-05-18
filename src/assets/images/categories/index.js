88d328d444015d0e763c4e69980b62be96b0079f
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-18 12:39:54 +0200

    [WIP] Create `index.js` with exports for images

---
+++ src/assets/images/categories/index.js
@@ -0,0 +1,6 @@
+import Capsules from "./capsules.svg";
+import Crew from "./crew.svg";
+import Details from "./details.svg";
+import Rockets from "./rockets.svg";
+
+export { Capsules, Crew, Details, Rockets };
---
+++ src/assets/images/rocket/index.js
@@ -0,0 +1,7 @@
+import Moon from "./moon.svg";
+import Rocket from "./rocket.svg";
+import SmokeLeft from "./smoke-left.svg";
+import SmokeRight from "./smoke-right.svg";
+import Wings from "./wings.svg";
+
+export { Moon, Rocket, SmokeLeft, SmokeRight, Wings };
