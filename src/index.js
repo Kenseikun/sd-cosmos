a96fb4ee7dd2dc592eeb80d5e8a688722d92da7d
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-18 10:26:59 +0200

    [WIP] Install and import `bootstrap`

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "./GlobalStyles/GlobalStyle";
 import { mainTheme } from "./GlobalStyles/theme/mainTheme";
+
 import Root from "./Root/Root";
 
 ReactDOM.render(
