44ed375043ff40a99badaebbbf0d37d508083f91
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-19 19:25:28 +0200

    [WIP] Import arrow svg icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,7 @@
 import Close from "./close.svg";
+import DownArrow from "./down-arrow.svg";
+import FilterArrow from "./filter-arrow.svg";
+import UpArrow from "./up-arrow.svg";
 
-export { Close };
+
+export { Close, DownArrow, FilterArrow, UpArrow };
