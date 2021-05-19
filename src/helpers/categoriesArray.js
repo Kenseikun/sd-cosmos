59da81bf3666109e0f89559fcd9bdb12b1addec6
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-19 22:17:55 +0200

    [WIP] Add object `categoriesNames`

---
+++ src/helpers/categoriesArray.js
@@ -0,0 +1,16 @@
+import { Capsules, Crew, Details, Rockets } from "../assets/images/categories";
+
+
+export const categoriesArray = [
+  { name: "Capsules", image: Capsules },
+  { name: "Crew", image: Crew },
+  { name: "Rockets", image: Details },
+  { name: "Starlink", image: Rockets },
+]
+
+export const categoriesNames = {
+  capsules: 'capsules',
+  crew: 'crew',
+  rockets: 'rockets',
+  starlink: 'starlink',
+}
