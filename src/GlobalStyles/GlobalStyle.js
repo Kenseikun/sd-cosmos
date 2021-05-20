0e51aea41c8b10716b5f2171435537dfce5cd916
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-20 14:03:13 +0200

    [WIP] Add `::before & ::after`

--- src/GlobalStyles/GlobalStyle.js
+++ src/GlobalStyles/GlobalStyle.js
@@ -10,11 +10,49 @@ const GlobalStyle = createGlobalStyle`
     }
 
     body {
+      overflow: hidden;
+
+      position: relative;
       color: white;
       height: 100vh;
       font-family: ${({ theme }) => theme.fontFamily.main};
       background: ${({ theme }) => theme.colors.background};
       background-repeat: no-repeat;
+
+    }
+
+    body::before {
+      position: absolute;
+      top: -150px;
+      left: -150px;
+
+      content: "";
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background-color: #00C9B7;
+
+      z-index: -20;
+
+      animation: mymove 5s linear infinite;
+    }
+
+    body::after {
+      position: absolute;
+      top: 150px;
+      right: -220px;
+
+      content: "";
+      width: 420px;
+      height: 420px;
+      border-radius: 50%;
+      border: 40px solid #272044;
+
+    }
+
+    @keyframes mymove {
+     0% {top: 620px;}
+     100% {left: 800px}
     }
 
     button {
