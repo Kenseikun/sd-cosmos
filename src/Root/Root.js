8c6d306fbb23ee06d0ed3215c2cddfcd7a018904
Sebastian Dziechciarz <sebastian.dziechciarz@movecloser.pl>
2021-05-21 10:38:06 +0200

    [WIP] Add filtration

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Categories from "../components/Categories";
 import RootContext from "../context";
-
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 import Rocket from "../components/Rocket";
-import { categoriesArray } from "../helpers/categoriesArray";
+import { categoriesArray, categoriesNames } from "../helpers/categoriesArray";
 
+import { DownArrow, UpArrow } from "../assets/icons";
 /**
  * @author Sebastian Dziechciarz
  */
 const Root = () => {
   const [categories, setCategories] = useState([...categoriesArray]);
 
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("crew");
   const [initialSpaceX, setInitialSpaceX] = useState([]);
   const [isDataLoading, setIsDataLoading] = useState(false);
   const [isRocketAnimate, setIsRocketAnimate] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [reverseSorting, setReversSorting] = useState(false);
 
   /**
    * Getting data from `SpaceX` API with dynamic `category`
@@ -53,9 +54,67 @@ const Root = () => {
    */
   const rocketAnimate = () => {
     setIsRocketAnimate(true);
-    setInterval(() => {
+    setTimeout(() => {
       setIsRocketAnimate(false);
-    }, 4000);
+    }, 3000);
+  };
+
+  const imgRef = useRef();
+
+  const sortAlphabetically = () => {
+    if (category === categoriesNames.crew) {
+      let sortedInitialSpaceX;
+      if (reverseSorting) {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => b.agency.localeCompare(a.agency));
+        setReversSorting(false);
+        imgRef.current.src = DownArrow;
+      } else {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => a.agency.localeCompare(b.agency));
+        setReversSorting(true);
+        imgRef.current.src = UpArrow;
+      }
+
+      setInitialSpaceX([...sortedInitialSpaceX]);
+    } else if (category === categoriesNames.starlink) {
+      let sortedInitialSpaceX;
+      if (reverseSorting) {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => b.version.localeCompare(a.version));
+        setReversSorting(false);
+        imgRef.current.src = DownArrow;
+      } else {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => a.version.localeCompare(b.version));
+        setReversSorting(true);
+        imgRef.current.src = UpArrow;
+      }
+
+      setInitialSpaceX([...sortedInitialSpaceX]);
+    } else if (category === categoriesNames.rockets) {
+      let sortedInitialSpaceX;
+      if (reverseSorting) {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => b.name.localeCompare(a.name));
+        setReversSorting(false);
+        imgRef.current.src = DownArrow;
+      } else {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => a.name.localeCompare(b.name));
+        setReversSorting(true);
+        imgRef.current.src = UpArrow;
+      }
+
+      setInitialSpaceX([...sortedInitialSpaceX]);
+    } else {
+      let sortedInitialSpaceX;
+      if (reverseSorting) {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => b.type.localeCompare(a.type));
+        setReversSorting(false);
+        imgRef.current.src = DownArrow;
+      } else {
+        sortedInitialSpaceX = initialSpaceX.sort((a, b) => a.type.localeCompare(b.type));
+        setReversSorting(true);
+        imgRef.current.src = UpArrow;
+      }
+
+      setInitialSpaceX([...sortedInitialSpaceX]);
+    }
   };
 
   return (
@@ -70,6 +129,8 @@ const Root = () => {
         rocketAnimate,
         setShowModal,
         showModal,
+        sortAlphabetically,
+        imgRef,
       }}
     >
       <div className="container d-flex flex-column" style={{ height: "100vh" }}>
